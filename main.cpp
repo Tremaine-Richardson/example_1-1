9790e9073dbc4ddfade0f98ae1aba9121f95e063
Tremaine-Richardson <trey7836@gmail.com>
2024-01-08 20:11:16 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+int main()
+{
+    DigitalIn gasDetector(D2);
+
+    DigitalOut alarmLed(LED1);
+
+    gasDetector.mode(PullDown);
+
+    alarmLed = OFF;
+
+    while (true) {
+        if ( gasDetector == ON ) {
+            alarmLed = ON;
+        } else {
+            alarmLed = ON;
+        }
+    }
+}
